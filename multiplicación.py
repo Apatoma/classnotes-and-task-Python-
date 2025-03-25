4a83b179fe4a9ee14975e2be28ddad29acd6145d
Sury <134803136+Apatoma@users.noreply.github.com>
2025-03-25 12:53:57 +0100

    Add files via upload

---
+++ multiplicación.py
@@ -0,0 +1,6 @@
+x = int(input("introduce el primer dígito: "))
+y = int(input("Introduce el priner dígito: "))
+
+multiplicacion = x * y 
+
+print("La multiplicación de", x , "y", y, "es:", multiplicacion)
