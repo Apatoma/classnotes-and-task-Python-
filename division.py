59372795094ac532590a1931e743a9f8827b5b4d
Sury <134803136+Apatoma@users.noreply.github.com>
2025-03-26 07:48:41 +0000

    upload files

---
+++ division.py
@@ -0,0 +1,6 @@
+x = float(input("Introduce el primer dígito: "))
+y = float(input("Introduce el segundo dígito: "))
+
+division = x/y
+
+print("La division de", x, "y", y, "es:", division)
---
+++ resta.py
@@ -0,0 +1,6 @@
+x = int(input("Introduce el primer dígito: "))
+y = int(input("Introduce el segundo dígito: "))
+
+resta = x - y
+
+print("La resta de", x, "y", y, "es:", resta)
