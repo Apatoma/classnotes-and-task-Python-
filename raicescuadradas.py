a67787663dbb5d1d3f051449c68ce155370020b0
Apatoma <alejandro.sanchez.infantes@proyectocartama.es>
2025-03-29 20:17:15 +0100

    more lines of code

--- raicescuadradas.py
+++ raicescuadradas.py
@@ -1,12 +1,28 @@
 import math
-a=float(input("Introduce coeficiente A: "))
-b=float(input("Introduce coeficiente B: "))
-c=float(input("Introduce coeficiente C: "))
-discriminante=b**2 - 4*a*c
-X1=(-b+math.sqrt(discriminante))/(2*a)
-X2=(-b-math.sqrt(discriminante))/(2*a)
-print("X1="+str(X1))
-print("X2="+str(X2))
+import cmath  # Para manejar números complejos
+
+a = float(input("Introduce coeficiente A: "))
+b = float(input("Introduce coeficiente B: "))
+c = float(input("Introduce coeficiente C: "))
+
+if a == 0:
+    # Ecuación lineal
+    if b != 0:
+        x = -c / b
+        print("Ecuación lineal: x =", x)
+    else:
+        print("No es una ecuación válida.")
+else:
+    discriminante = b**2 - 4*a*c
+    if discriminante >= 0:
+        X1 = (-b + math.sqrt(discriminante)) / (2*a)
+        X2 = (-b - math.sqrt(discriminante)) / (2*a)
+    else:
+        # Usar cmath.sqrt para obtener raíces complejas
+        X1 = (-b + cmath.sqrt(discriminante)) / (2*a)
+        X2 = (-b - cmath.sqrt(discriminante)) / (2*a)
+    print("X1 =", X1)
+    print("X2 =", X2)
 
 #este programa sirve para calcular las raices cuadradas de una ecuacion de segundo grado, es decir, una ecuacion de la forma ax^2 + bx + c = 0.
 #Para ello, se le piden al usuario los coeficientes a, b y c, y se calcula el discriminante.
