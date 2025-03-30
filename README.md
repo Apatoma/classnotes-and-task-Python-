4239920a7f1b05217107d818d60240de555dc9ae
Apatoma <alejandro.sanchez.infantes@proyectocartama.es>
2025-03-30 18:51:30 +0200

    si

--- README.md
+++ README.md
@@ -58,10 +58,6 @@ Los bloques if-else del ejercicio 5 son mejores ya que tienen capacidad para otr
 Me dice que el nombre math de la libreria math no existe y que si se me ha olvidado importar la libreria.
 
 ***Escribe # a la izquierda de la instrucción discriminante=b**2 - 4*a*c***
-<<<<<<< HEAD
-=======
-
->>>>>>> bc02dcf92eaa151cde8b757f53e91afbfa59765c
 No puede hacer el calculo del discriminante, como ha dejado de existir pero sigue incluido en otras lineas de codigo no es capaz de dar un output final.
 
 ***Quita str de la última instrucción.***
@@ -111,10 +107,7 @@ Debo tener el codigo alineado con las instrucciones, en el caso contrario, da er
 
 
 
-<<<<<<< HEAD
+
 ***PERDON POR EL POCO USO DE TILDES, PROGRAMO EN EL TECLADO DE REINO UNIDO PORQUE ME RESULTA MAS FACIL Y AUN MAS CON UNA MANO***
 
-
-=======
-***PERDON POR EL POCO USO DE TILDES, PROGRAMO EN EL TECLADO DE REINO UNIDO PORQUE ME RESULTA MAS FACIL Y AUN MAS CON UNA MANO.***
->>>>>>> 9a179ea269731855ff3235fe3e8d2b5e005b8ec2
+admito haber tenido la ayuda de autocompletado de copilot que ha agilizado mi codigo ademas de darme alguna idea.
--- delegado.py
+++ delegado.py
@@ -1,36 +1,37 @@
-import numpy as np # type: ignore
+import numpy as np    
+candidatos = np.empty(5, dtype=object)
 
-candidatos = np.array[1:5] 
+candidatos[0] = input("Introduce el nombre del candidato 1: ")
+candidatos[1] = input("Introduce el nombre del candidato 2: ")
+candidatos[2] = input("Introduce el nombre del candidato 3: ")
+candidatos[3] = input("Introduce el nombre del candidato 4: ")
+candidatos[4] = input("Introduce el nombre del candidato 5: ")
 
-candidatos[1] = int(input("Introduce el nombre del candidato 1: "))
-candidatos[2] = int(input("Introduce el nombre del candidato 2: "))
-candidatos[3] = int(input("Introduce el nombre del candidato 3: "))
-candidatos[4] = int(input("Introduce el nombre del candidato 4: "))
-candidatos[5] = int(input("Introduce el nombre del candidato 5: "))
-
-votos = np.array[0, 0, 0, 0]
+votos = np.zeros(5, dtype=int)
 
 votos[0] = int(input("Introduce el número de votos del candidato 1: "))
 votos[1] = int(input("Introduce el número de votos del candidato 2: "))
 votos[2] = int(input("Introduce el número de votos del candidato 3: "))
 votos[3] = int(input("Introduce el número de votos del candidato 4: "))
+votos[4] = int(input("Introduce el número de votos del candidato 5: "))
+
 print("Los votos son:", votos)
 print("Los candidatos son:", candidatos)
 print("El candidato 1 tiene", votos[0], "votos.")
 print("El candidato 2 tiene", votos[1], "votos.")
 print("El candidato 3 tiene", votos[2], "votos.")
 print("El candidato 4 tiene", votos[3], "votos.")
+print("El candidato 5 tiene", votos[4], "votos.")
 
-if votos[0] > votos[1] and votos[0] > votos[2] and votos[0] > votos[3]:
+if votos[0] > votos[1] and votos[0] > votos[2] and votos[0] > votos[3] and votos[0] > votos[4]:
     print("El candidato 1 ha ganado.")
-elif votos[1] > votos[0] and votos[1] > votos[2] and votos[1] > votos[3]:
+elif votos[1] > votos[0] and votos[1] > votos[2] and votos[1] > votos[3] and votos[1] > votos[4]:
     print("El candidato 2 ha ganado.")
-elif votos[2] > votos[0] and votos[2] > votos[1] and votos[2] > votos[3]:
+elif votos[2] > votos[0] and votos[2] > votos[1] and votos[2] > votos[3] and votos[2] > votos[4]:
     print("El candidato 3 ha ganado.")
-elif votos[3] > votos[0] and votos[3] > votos[1] and votos[3] > votos[2]:
+elif votos[3] > votos[0] and votos[3] > votos[1] and votos[3] > votos[2] and votos[3] > votos[4]:
     print("El candidato 4 ha ganado.")
-elif votos[4] > votos[0] and votos[4] > votos[1] and votos[4] > votos[2]:
+elif votos[4] > votos[0] and votos[4] > votos[1] and votos[4] > votos[2] and votos[4] > votos[3]:
     print("El candidato 5 ha ganado.")
-
 else:
     print("Ha habido un empate.")
