b0b0eeb72296249043d31183551e1194b6aeb0aa
Sury <134803136+Apatoma@users.noreply.github.com>
2025-05-13 10:47:20 +0000

    hola

--- delegado.py
+++ delegado.py
@@ -9,11 +9,12 @@ candidatos[4] = input("Introduce el nombre del candidato 5: ")
 
 votos = np.zeros(5, dtype=int)
 
-votos[0] = int(input("Introduce el número de votos del candidato 1: "))  
-votos[1] = int(input("Introduce el número de votos del candidato 2: "))
-votos[2] = int(input("Introduce el número de votos del candidato 3: "))
-votos[3] = int(input("Introduce el número de votos del candidato 4: "))
-votos[4] = int(input("Introduce el número de votos del candidato 5: "))
+while True !=0:
+    votos[0] = int(input("Introduce el número de votos del candidato 1: "))  
+    votos[1] = int(input("Introduce el número de votos del candidato 2: "))
+    votos[2] = int(input("Introduce el número de votos del candidato 3: "))
+    votos[3] = int(input("Introduce el número de votos del candidato 4: "))
+    votos[4] = int(input("Introduce el número de votos del candidato 5: "))
 
 print("Los votos son:", votos)
 print("Los candidatos son:", candidatos)
@@ -23,15 +24,12 @@ print("El candidato 3 tiene", votos[2], "votos.")
 print("El candidato 4 tiene", votos[3], "votos.")
 print("El candidato 5 tiene", votos[4], "votos.")
 
-if votos[0] > votos[1] and votos[0] > votos[2] and votos[0] > votos[3] and votos[0] > votos[4]:
-    print("El candidato 1 ha ganado.")
-elif votos[1] > votos[0] and votos[1] > votos[2] and votos[1] > votos[3] and votos[1] > votos[4]:
-    print("El candidato 2 ha ganado.")
-elif votos[2] > votos[0] and votos[2] > votos[1] and votos[2] > votos[3] and votos[2] > votos[4]:
-    print("El candidato 3 ha ganado.")
-elif votos[3] > votos[0] and votos[3] > votos[1] and votos[3] > votos[2] and votos[3] > votos[4]:
-    print("El candidato 4 ha ganado.")
-elif votos[4] > votos[0] and votos[4] > votos[1] and votos[4] > votos[2] and votos[4] > votos[3]:
-    print("El candidato 5 ha ganado.")
-else:
-    print("Ha habido un empate.")
+mayor_votos = 0
+ganador = 0
+
+for i in range(5):
+    if votos[i] > mayor_votos:
+        mayor_votos = votos[i]
+        ganador = candidatos[i]
+
+print("El ganador es", ganador, "con", mayor_votos, "votos.")
