93c36c87587dd7d7cade978506c3fddcda923200
Sury <134803136+Apatoma@users.noreply.github.com>
2025-05-21 18:18:04 +0000

    actualizacion mediocre

--- delegado.py
+++ delegado.py
@@ -3,7 +3,6 @@ candidatos = []
 votos = []
 nombre = -1
 
-
 while nombre != "":
     nombre = input("Introduce el nombre del candidato o pulsa ENTER para terminar: ")
     if nombre != "":  
@@ -17,11 +16,16 @@ print("Los votos son:", votos)
 print("Los candidatos son:", candidatos)
 
 mayor_votos = 0
-ganador = ""
+ganadores = []
 
 for i in range(len(candidatos)):
     if votos[i] > mayor_votos:
         mayor_votos = votos[i]
-        ganador = candidatos[i]
+        ganadores = [candidatos[i]]
+    elif votos[i] == mayor_votos:
+        ganadores.append(candidatos[i])
 
-print("El ganador es", ganador, "con", mayor_votos, "votos.")
+if len(ganadores) == 1:
+    print("El ganador es", ganadores[0], "con", mayor_votos, "votos.")
+else:
+    print("Hay un empate entre:", ", ".join(ganadores), "con", mayor_votos, "votos.")
